766cc30cebecf4cc0c8acf25c82a49f08329d70f
alextjanse <github@alexjanse.nl>
2025-10-07 13:21:35 +0200

    Solution to 31

---
+++ 31.py
@@ -0,0 +1,22 @@
+counter = 0
+
+states = [(200, [1, 2, 5, 10, 20, 50, 100, 200])]
+
+while states:
+    rem, coins = states.pop()
+
+    if rem == 0:
+        counter += 1
+        continue
+
+    if len(coins) == 0:
+        # no coins remaining, but there is a remainder
+        continue
+
+    coin = coins.pop()
+    n = 0
+    while n * coin <= rem:
+        states.append((rem - n * coin, list(coins)))
+        n += 1
+
+print(counter)
