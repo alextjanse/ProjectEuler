b4959908c6c8655d7c929b60d5d70300b63d5f94
alextjanse <github@alexjanse.nl>
2025-10-18 01:11:39 +0200

    add prime factors

--- lib/primes.py
+++ lib/primes.py
@@ -1,39 +1,58 @@
 '''Prime module, with different functions for prime calculations.'''
 
-primes = [2, 3]
+from collections import Counter
+
+prime_list = [2, 3]
+prime_set = set(prime_list)
 
 def is_prime(n: int) -> bool:
-    p_max = primes[-1]
+    p_max = prime_list[-1]
     
     if n <= p_max:
-        return n in primes
+        return n in prime_set
     
     if p_max * p_max >= n:
-        for p in primes:
+        for p in prime_list:
             if p * p > n:
                 break
             if n % p == 0:
                 return False
         return True
-
-    for p in next_prime():
-        if p < n:
-            continue
-        return p == n
     
-    return False
+    prime_gen = next_prime()
+    while p_max < n:
+        p_max = next(prime_gen)
+    
+    return p_max == n
 
 def next_prime():
-    candidate = primes[-1] + 2
+    p = prime_list[-1] + 2
     while True:
-        if is_prime(candidate):
-            primes.append(candidate)
-            yield candidate
-        candidate += 2
+        if is_prime(p):
+            prime_list.append(p)
+            prime_set.add(p)
+            yield p
+        p += 2
 
-def get_primes():
-    for p in primes:
+def primes():
+    for p in prime_list:
         yield p
     
     for p in next_prime():
         yield p
+
+def prime_factors(n: int) -> dict[int, int]:
+    factors = Counter()
+
+    for p in primes():
+        if n == 1:
+            break
+
+        while n % p == 0:
+            factors[p] += 1
+            n //= p
+
+    return dict(factors)
+
+if __name__ == "__main__":
+    print(prime_factors(12))
