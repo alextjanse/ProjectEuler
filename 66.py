57d7f25bc2d00edcc75b708b995a11f0c8d07e3e
alextjanse <github@alexjanse.nl>
2025-10-19 03:23:07 +0200

    66 attempt 2

--- 66.py
+++ 66.py
@@ -1,24 +1,28 @@
 from math import isqrt
+# from gmpy2 import is_square
 
 def is_square(n: int) -> bool:
     root = isqrt(n)
     return root * root == n
 
-def solve(d_max = 1000):
-    not_found_ds = { d for d in range(1, d_max + 1) if not is_square(d) }
+def solve(d_max: int = 1000) -> int:
+    ds = { d for d in range(1, d_max + 1) if not is_square(d) }
+
+    x2_max, d_max, y = 0, 0, 1
+    while ds:
+        for d in set(ds):
+            x2 = (d * y * y) + 1
+            if is_square(x2):
+                ds.remove(d)
+                print(isqrt(x2), '-', d, y, len(ds))
+                if x2 > x2_max:
+                    x2_max, d_max = x2, d
+        y += 1
     
-    x = 2
-    while not_found_ds:
-        for d in set(not_found_ds):
-            dy2 = x * x - 1
-            if dy2 % d == 0 and is_square(dy2 // d):
-                y = isqrt(dy2 // d)
-                print(x, d, y, ':', x * x, '-', d, y * y, '=', 1, ';', len(not_found_ds) - 1, 'remaining')
+    y2_max = (x2_max - 1) // d_max
+    print(isqrt(x2_max), d_max, isqrt(y2_max), x2_max, y2_max, x2_max - d_max * y2_max)
 
-                not_found_ds.remove(d)
-        x += 1
-
-    print(x)
+    return isqrt(x2_max)
 
 if __name__ == "__main__":
     solve()
