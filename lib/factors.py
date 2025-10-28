418c632d8fd48a5bd99af2c82fc0730b6c6aac82
alextjanse <github@alexjanse.nl>
2025-10-28 21:41:41 +0100

    moved factors and added get_factors

---
+++ lib/factors.py
@@ -0,0 +1,67 @@
+from functools import reduce
+from math import isqrt
+import operator
+from lib.primes import primes
+from collections import Counter, defaultdict
+
+def factorize(n: int) -> dict[int, int]:
+    factors: Counter[int] = Counter()
+
+    for p in primes():
+        if n == 1:
+            break
+
+        while n % p == 0:
+            factors[p] += 1
+            n //= p
+
+    return factors
+
+def reduce_factors(factors: dict[int, int]) -> int:
+    return reduce(operator.mul, map(lambda f: f[0] ** f[1], factors.items()), 1)
+
+def phi(n: int) -> int:
+    if n < 1: raise ValueError(n, "has to be larger than 0")
+    elif n == 1: return 1
+    x = 1
+    for p, m in factorize(n).items():
+        x *= p ** (m - 1) * (p - 1)
+    return x
+
+'''
+Get the prime factors of all numbers up to n.
+'''
+def get_factors(n: int) -> dict[int, Counter[int]]:
+    factors: dict[int, Counter[int]] = defaultdict(Counter)
+
+    active_numbers: set[int] = set()
+    root_n = isqrt(n)
+    for p in primes():
+        if p > root_n:
+            break
+
+        for i in set(active_numbers):
+            fac_n = factors[i]
+            if i * p > n:
+                # next p will also be bigger than max, remove it
+                active_numbers.remove(i)
+                continue
+
+            new_factors = Counter(fac_n)
+            m = i * p
+            while m < n:
+                new_factors[p] += 1
+                factors[m] = Counter(new_factors) # Copy
+                active_numbers.add(m)
+                m *= p
+
+        # add p and powers of p
+        new_factors = Counter([p])
+        i = p
+        while i < n:
+            factors[i] = Counter(new_factors)
+            active_numbers.add(i)
+            i *= p
+            new_factors[p] += 1
+
+    return factors
--- lib/primes.py
+++ lib/primes.py
@@ -37,30 +37,6 @@ def primes():
     for p in next_prime():
         yield p
 
-def prime_factors(n: int) -> dict[int, int]:
-    factors: Counter[int] = Counter()
-
-    for p in primes():
-        if n == 1:
-            break
-
-        while n % p == 0:
-            factors[p] += 1
-            n //= p
-
-    return factors
-
-def reduce_factors(factors: dict[int, int]) -> int:
-    return reduce(operator.mul, map(lambda f: f[0] ** f[1], factors.items()), 1)
-
-def phi(n: int) -> int:
-    if n < 1: raise ValueError(n, "has to be larger than 0")
-    elif n == 1: return 1
-    x = 1
-    for p, m in prime_factors(n).items():
-        x *= p ** (m - 1) * (p - 1)
-    return x
-
 if __name__ == "__main__":
     for i in range(100):
         if is_prime(i):
