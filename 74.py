331b3bde2dcae34c084013c7558e7a70d58530fc
alextjanse <github@alexjanse.nl>
2025-11-01 18:48:03 +0100

    74 solved

--- 74.py
+++ 74.py
@@ -1,5 +1,4 @@
 from math import factorial
-from lib.graphs import DiGraph
 
 def digit_factorial(n: int) -> int:
     return sum(map(lambda c: factorial(int(c)), str(n)))
@@ -9,16 +8,13 @@ def solve():
     l_2 = { 871, 45361, 872, 45352 }
     l_3 = { 169, 363601, 1454 }
 
-    g = DiGraph()
     lengths: dict[int, int] = {
         0: 2, # 0 -> 1 -> 1
         1: 1, # 1 -> 1
         2: 1, # 2 -> 2
-        145: 1, # 145 => 145
-        40585: 1, # Found while debugging
-    }
-    lengths.update({ i : 2 for i in l_2 })
-    lengths.update({ i : 3 for i in l_3 })
+        145: 1, # 145 -> 145
+        40585: 1, # 40585 -> 40585, found while debugging
+    } | { i : 2 for i in l_2 } | { i : 3 for i in l_3 }
 
     count = 0
 
@@ -29,7 +25,6 @@ def solve():
         chain = [s]
         while t not in lengths:
             r = digit_factorial(t)
-            g.add_edge(t, r)
             chain.append(r)
             t = r
         length_t = lengths.get(t, 1)
@@ -37,6 +32,7 @@ def solve():
             lengths[t] = length_t + i
         if lengths[s] == 60:
             count += 1
+
     return count
 
 if __name__ == "__main__":
